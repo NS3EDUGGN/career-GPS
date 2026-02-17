3aadd8c294b3b81ad2e141b92db5ba2f786c72f5
NS3EDUGGN <info@ns3edu.com>
2026-02-17 16:48:00 +0530

    Update StartTestForm.jsx

--- src/pages/StartTestForm.jsx
+++ src/pages/StartTestForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect  } from "react"
 import { useNavigate, Link } from "react-router-dom"
 
 function StartTestForm() {
@@ -183,4 +183,5 @@ function StartTestForm() {
 }
 
 export default StartTestForm
+
 
