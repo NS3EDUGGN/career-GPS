0b29a41db545b28af70125623392f0bc2a9c66fa
NS3EDUGGN <info@ns3edu.com>
2026-02-24 17:12:21 +0530

    Update register.js

--- api/register.js
+++ api/register.js
@@ -1,70 +1,34 @@
-import mongoose from "mongoose"
-
-const MONGO_URI = process.env.MONGO_URI
-
-let isConnected = false
-
-async function connectDB() {
-  if (isConnected) return
-
- await mongoose.connect(MONGO_URI, {
-  
-
-  isConnected = true
-}
+import { connectDB } from "./_db.js"
+import User from "./models/User.js"
 
 export default async function handler(req, res) {
 
-  if (req.method !== "POST") {
+  if (req.method !== "POST")
     return res.status(405).json({ message: "Only POST allowed" })
-  }
 
   try {
 
     await connectDB()
 
-    // create schema AFTER connection
-    const userSchema = new mongoose.Schema({
-      name: String,
-      email: String,
-      password: String,
-      testGiven: { type: Boolean, default: false },
-      scores: Object
-    })
-
-    const User =
-      mongoose.models.User || mongoose.model("User", userSchema)
-
-   const { name, email, password, phone, interest, other_interest } = req.body
-
+    const { name, email, password, phone } = req.body
 
     const exists = await User.findOne({ email })
+
     if (exists) {
       return res.status(400).json({ message: "User already exists" })
     }
 
-    await User.create({ name, email, password })
-    // send to google sheet
-    await fetch("https://script.google.com/macros/s/AKfycbwmm2CNZaisE_EyRDd7zhILgHlE6c7lMaN-OZmlPrAI8sfODCPC7J19IyP_73XNw9B6/exec", {
-  method: "POST",
-  headers: {
-    "Content-Type": "text/plain;charset=utf-8"
-  },
-  body: JSON.stringify({
-    name: name,
-    email: email,
-    phone: phone,
-    interest: interest,
-    other: other_interest
-  })
-})
+    await User.create({
+      name,
+      email,
+      password,
+      phone
+    })
 
-
-
-    return res.status(200).json({ message: "Registered Successfully" })
+    return res.status(200).json({ success: true })
 
   } catch (err) {
-    console.log(err)   // VERY IMPORTANT (shows in Vercel logs)
-    return res.status(500).json({ message: err.message })
+    console.log("REGISTER ERROR:", err)
+    return res.status(500).json({ message: "Server error" })
   }
 }
